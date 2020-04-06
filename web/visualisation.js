d0637ad0ba22a51ef766b1767be47302802b5dfd
Bradley Grantham <bgrantham@channel4.co.uk>
2020-04-06 15:34:40 +0100

    :recycle: Refactor a bunch of js code into more concise methods

--- web/visualisation.js
+++ web/visualisation.js
@@ -1,39 +1,42 @@
-var origin = [$(window).width() / 2, $(window).height() / 1.4], j = 10, scale = 20,
-    keypoints = [], yLine = [], xGrid = [],
-    yGrid1 = [], yGrid2 = [], line_data = [], beta = 0, alpha = 0, key = function (d) {
-        return d.id;
-    }, startAngle = 0;
+const origin = [$(window).width() / 2, $(window).height() / 1.4];
+const j = 10;
+const scale = 20;
+var keypoints = [], line_data = [], xGrid_data = [], yGrid1_data = [], yGrid2_data = [];
+
 var svg = d3.select('svg.main').append('g').attr("class", "stickman");
-var cnt2;
-var mx, my, mouseX, mouseY;
 
-const parents = [-1, 0, 1, 2, 0, 4, 5, 0, 7, 8, 9, 8, 11, 12, 8, 14, 15];
+var frameNumber;
+var mx, my, mouseX, mouseY, beta = 0, alpha = 0, startAngle = Math.PI / 4;
 
+const parent_joints = [-1, 0, 1, 2, 0, 4, 5, 0, 7, 8, 9, 8, 11, 12, 8, 14, 15];
 const joints_left = [4, 5, 6, 11, 12, 13];
 const joints_right = [1, 2, 3, 14, 15, 16];
 
-var grid3dx = d3._3d()
+//mimic python range() method
+let range = n => Array.from(Array(n).keys());
+
+let grid3dx = d3._3d()
     .shape('GRID', 20)
     .origin(origin)
     .rotateY(startAngle)
     .rotateX(-startAngle)
     .scale(scale);
 
-var grid3dy1 = d3._3d()
+let grid3dy1 = d3._3d()
     .shape('GRID', 20)
     .origin(origin)
     .rotateY(startAngle)
     .rotateX(-startAngle)
     .scale(scale);
 
-var grid3dy2 = d3._3d()
+let grid3dy2 = d3._3d()
     .shape('GRID', 20)
     .origin(origin)
     .rotateY(startAngle)
     .rotateX(-startAngle)
     .scale(scale);
 
-var point3d = d3._3d()
+let point3d = d3._3d()
     .x(function (d) {
         return d.x;
     })
@@ -48,8 +51,7 @@ var point3d = d3._3d()
     .rotateX(-startAngle)
     .scale(scale * 10);
 
-
-var line3d = d3._3d()
+let line3d = d3._3d()
     .shape('LINE')
     .x(function (d) {
         return d.x;
@@ -65,11 +67,9 @@ var line3d = d3._3d()
     .rotateX(-startAngle)
     .scale(scale * 10);
 
-function processData(data) {
 
-    /* ----------- GRID ----------- */
-
-    var xGrid = svg.selectAll('path.xgrid').data(data[0], key);
+function createXGrid(data) {
+    var xGrid = svg.selectAll('path.xgrid').data(data);
 
     xGrid
         .enter()
@@ -85,8 +85,10 @@ function processData(data) {
         .attr('d', grid3dx.draw);
 
     xGrid.exit().remove();
+}
 
-    var yGrid1 = svg.selectAll('path.ygrid1').data(data[2], key);
+function createYGrid1(data) {
+    var yGrid1 = svg.selectAll('path.ygrid1').data(data);
 
     yGrid1
         .enter()
@@ -102,9 +104,10 @@ function processData(data) {
         .attr('d', grid3dy1.draw);
 
     yGrid1.exit().remove();
+}
 
-
-    var yGrid2 = svg.selectAll('path.ygrid2').data(data[3], key);
+function createYGrid2(data) {
+    var yGrid2 = svg.selectAll('path.ygrid2').data(data);
 
     yGrid2
         .enter()
@@ -120,10 +123,10 @@ function processData(data) {
         .attr('d', grid3dy2.draw);
 
     yGrid2.exit().remove();
+}
 
-    /* ----------- POINTS ----------- */
-
-    var points = svg.selectAll('circle').data(data[1], key);
+function createPoints(data) {
+    var points = svg.selectAll('circle').data(data);
 
     points
         .enter()
@@ -137,7 +140,6 @@ function processData(data) {
             return d.projected.y
         })
         .merge(points)
-        // .transition().duration(10)
         .attr('r', 3)
         .attr('stroke', function (d) {
             return d3.color('red');
@@ -154,10 +156,10 @@ function processData(data) {
         });
 
     points.exit().remove();
+}
 
-    /* ----------- lines ----------- */
-
-    var lines = svg.selectAll('line').data(data[4]);
+function createLines(data) {
+    var lines = svg.selectAll('line').data(data);
 
     lines
         .enter()
@@ -165,7 +167,6 @@ function processData(data) {
         .attr('class', '_3d')
         .attr('stroke-width', 1)
         .merge(lines)
-        // .transition().duration(10)
         .attr('fill', function (d) {
             return d3.color(d[0].colour);
         })
@@ -186,39 +187,54 @@ function processData(data) {
         });
 
     lines.exit().remove();
+}
+
+function processData(data) {
+
+    createXGrid(data[0]);
+
+    createYGrid1(data[2]);
+
+    createYGrid2(data[3]);
+
+    createPoints(data[1]);
+
+    createLines(data[4]);
 
     d3.selectAll('._3d').sort(d3._3d().sort);
 }
 
 
 function drawStickman() {
-    var cnt = 0;
-    xGrid = [], yGrid1 = [], yGrid2 = [], keypoints = [], yLine = [];
+    // create grid data
+    xGrid_data = [], yGrid1_data = [], yGrid2_data = [];
     for (var z = -j; z < j; z++) {
         for (var x = -j; x < j; x++) {
-            xGrid.push([x, 0, z]);
-            yGrid1.push([9, x - 9, z]);
-            yGrid2.push([x, z - 9, -10]);
+            xGrid_data.push([x, 0, z]);
+            yGrid1_data.push([9, x - 9, z]);
+            yGrid2_data.push([x, z - 9, -10]);
 
         }
     }
 
-    console.log(cnt2);
-    golf_swing[cnt2].forEach((element) => {
+
+    // create keypoints data
+    keypoints = [];
+    swing_keypoints[frameNumber].forEach((element) => {
         keypoints.push({
             x: element[0],
             y: element[2],
             z: element[1],
-            id: 'point_' + cnt++
         })
     });
 
+    // create line data
     line_data = [];
-    for (const [index, start_point] of golf_swing[cnt2].entries()) {
-        var parent = parents[index];
+    for (const [index, start_point] of swing_keypoints[frameNumber].entries()) {
+        let parent = parent_joints[index];
         if (parent !== -1) {
-            var end_point = golf_swing[cnt2][parent];
-            var line_colour = (joints_left.includes(index) || joints_left.includes(parent)) ? 'blue' : 'black';
+            let end_point = swing_keypoints[frameNumber][parent];
+            let line_colour = (joints_left.includes(index) || joints_left.includes(parent)) ? 'blue' : 'black';
             line_data.push(
                 [
                     {
@@ -238,14 +254,13 @@ function drawStickman() {
 
     }
 
-    var data = [
-        grid3dx(xGrid),
+    let data = [
+        grid3dx(xGrid_data),
         point3d(keypoints),
-        grid3dy1(yGrid1),
-        grid3dy2(yGrid2),
+        grid3dy1(yGrid1_data),
+        grid3dy2(yGrid2_data),
         line3d(line_data),
     ];
-    cnt2++;
     processData(data);
 }
 
@@ -259,11 +274,11 @@ function dragged() {
     mouseY = mouseY || 0;
     beta = (d3.event.x - mx + mouseX) * Math.PI / 230;
     alpha = (d3.event.y - my + mouseY) * Math.PI / 230 * (-1);
-    var data = [
-        grid3dx.rotateY(beta + startAngle).rotateX(alpha - startAngle)(xGrid),
+    let data = [
+        grid3dx.rotateY(beta + startAngle).rotateX(alpha - startAngle)(xGrid_data),
         point3d.rotateY(beta + startAngle).rotateX(alpha - startAngle)(keypoints),
-        grid3dy1.rotateY(beta + startAngle).rotateX(alpha - startAngle)(yGrid1),
-        grid3dy2.rotateY(beta + startAngle).rotateX(alpha - startAngle)(yGrid2),
+        grid3dy1.rotateY(beta + startAngle).rotateX(alpha - startAngle)(yGrid1_data),
+        grid3dy2.rotateY(beta + startAngle).rotateX(alpha - startAngle)(yGrid2_data),
         line3d.rotateY(beta + startAngle).rotateX(alpha - startAngle)(line_data),
     ];
     processData(data);
@@ -274,20 +289,16 @@ function dragEnd() {
     mouseY = d3.event.y - my + mouseY;
 }
 
-//mimic python range() method
-let range = n => Array.from(Array(n).keys());
+var swing_keypoints = [];
+$.getJSON('data.json', function (data) {
+    swing_keypoints = data;
 
-var golf_swing = [];
-$.getJSON('data.json', function(data) {
-    cnt2 = 0;
-    golf_swing = data;
+    frameNumber = 0;
+    drawStickman();
 
     sliderInit(data.length);
-
     // drag stick man
     d3.select("g.stickman").call(d3.drag().on('drag', dragged).on('start', dragStart).on('end', dragEnd));
-
-    drawStickman();
 
 });
 
@@ -298,23 +309,22 @@ function sliderInit(sliderSteps) {
     .min(d3.min(range(sliderStep)))
     .max(d3.max(range(sliderSteps)))
     .width(300)
-    .tickFormat('')
-    // .ticks(5)
     .step(1)
     .default(0)
     .on('onchange', val => {
-      cnt2 = val;
+      frameNumber = val;
       drawStickman();
     });
 
-    var slider = d3
-    .select('div#slider-step')
-    .append('svg')
-    .attr('class', 'slider')
-    .attr('width', 500)
-    .attr('height', 100)
-    .append('g')
-    .attr('transform', 'translate(30,30)');
+    const slider = d3
+        .select('div#slider-step')
+        .append('svg')
+        .attr('class', 'slider')
+        .attr('width', 500)
+        .attr('height', 100)
+        .append('g')
+        .attr('class', 'slider')
+        .attr('transform', 'translate(30,30)');
 
     // drag slider
     slider.call(sliderStep);
